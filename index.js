5410bcb2f1d88b5cc29484a19bd9bb32dd17d486
Lara <laraellenh@gmail.com>
2022-01-13 17:03:42 -0500

    commit tree 2be8623c2537c4f2dd853a6f54156bcc58f79830

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+// in each case, return declares a new variable (sName, isItAMatch, Matches)
+
+function findMatching(drivers, name) {
+    return drivers.filter(sName => {
+        return (sName.toUpperCase() === name.toUpperCase())
+    })
+}
+// filter for the first less matching, regardless of case. Change B to b for example, at index 0 (first letter)
+function fuzzyMatch(drivers, firsLetterofNameMatch) {
+    return drivers.filter(isItAMatch =>
+        isItAMatch.toLowerCase().indexOf(firsLetterofNameMatch.toLowerCase()) === 0
+    );
+}
+// filter through the name value for matches
+function matchName(drivers, matchname) {
+    return drivers.filter((matches) => matches.name === matchname);
+}
